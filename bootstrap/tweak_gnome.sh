791c25f2c9c0123d8a8a6852a27ab7771e16d8a7
Oriol Soriano <oriolsoriano@gmail.com>
2018-12-02 21:58:29 +0100

    Install gnome-tweak-tool and gnome extensions

--- bootstrap/tweak_gnome.sh
+++ bootstrap/tweak_gnome.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+sudo apt install gnome-tweak-tool
+sudo apt install gnome-shell-extensions
+
 # Minimize windows when clicking its icon in the launcher
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
