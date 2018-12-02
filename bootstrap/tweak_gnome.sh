b14c36d8e15316531f73a9f7ee5e90354a41d809
Oriol Soriano <oriolsoriano@gmail.com>
2018-12-02 21:58:52 +0100

    Install and configure Numix theme

--- bootstrap/tweak_gnome.sh
+++ bootstrap/tweak_gnome.sh
@@ -3,6 +3,14 @@
 sudo apt install gnome-tweak-tool
 sudo apt install gnome-shell-extensions
 
+# Numix theme
+sudo add-apt-repository ppa:numix/ppa
+sudo apt update
+sudo apt install numix-gtk-theme numix-icon-theme-circle
+gsettings set org.gnome.desktop.interface gtk-theme "Numix"
+gsettings set org.gnome.desktop.wm.preferences theme "Numix"
+gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
+
 # Minimize windows when clicking its icon in the launcher
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
