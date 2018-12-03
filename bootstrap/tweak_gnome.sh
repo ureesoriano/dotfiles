ee5d4650710fc8176db7cf4d0cdab38f9f568dd0
Oriol Soriano <oriolsoriano@gmail.com>
2018-12-03 01:01:33 +0100

    Add and configure 'dash to dock'

--- bootstrap/tweak_gnome.sh
+++ bootstrap/tweak_gnome.sh
@@ -11,6 +11,23 @@ gsettings set org.gnome.desktop.interface gtk-theme "Numix"
 gsettings set org.gnome.desktop.wm.preferences theme "Numix"
 gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
 
-# Minimize windows when clicking its icon in the launcher
+# Install dash to dock gnome extension
+wget https://extensions.gnome.org/review/download/8355.shell-extension.zip
+unzip ./8355.shell-extension.zip\
+    -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/
+
+# Minimize windows when clicking its icon in the Dash
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
+# Dash max icon size: 42px
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 42
+gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true
+
+# Applications active on Dash only in the workspace they are on
+gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
+
+# Shrink Dash
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+
+# Window counter indicators -> dashes
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DASHES'
