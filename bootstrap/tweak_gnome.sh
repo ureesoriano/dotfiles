a7ece8b2e8f09532fb002f798b35c21b9f2c893a
Oriol Soriano <oriolsoriano@gmail.com>
2018-12-05 22:31:58 +0100

    Add OpenWeather gnome extension

--- bootstrap/tweak_gnome.sh
+++ bootstrap/tweak_gnome.sh
@@ -55,3 +55,8 @@ unzip ./hidetopbar@mathieu.bidon.ca.v63.shell-extension.zip \
 wget https://extensions.gnome.org/extension-data/Move_Clock@rmy.pobox.com.v18.shell-extension.zip
 unzip ./Move_Clock@rmy.pobox.com.v18.shell-extension.zip \
     -d ~/.local/share/gnome-shell/extensions/Move_Clock@rmy.pobox.com/
+
+# Install OpenWeather gnome extension
+wget https://extensions.gnome.org/extension-data/openweather-extension@jenslody.de.v97.shell-extension.zip
+unzip ./openweather-extension@jenslody.de.v97.shell-extension.zip \
+    -d ~/.local/share/gnome-shell/extensions/openweather-extension@jenslody.de
