c17fc0a6700fd2c061efe62cdf6125707943a0ae
Oriol Soriano <oriolsoriano@gmail.com>
2018-12-06 20:20:43 +0100

    Add docker-compose

--- bootstrap/dev_tools.sh
+++ bootstrap/dev_tools.sh
@@ -29,5 +29,19 @@ function install_docker {
   sudo apt-get install -y docker-ce
 }
 
+function install_docker_compose {
+  sudo curl -L \
+    "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+
+  sudo chmod +x /usr/local/bin/docker-compose
+
+  # Add docker-compose bash completion
+  sudo curl -L \
+    https://raw.githubusercontent.com/docker/compose/1.23.1/contrib/completion/bash/docker-compose \
+    -o /etc/bash_completion.d/docker-compose
+}
+
 install_essentials
 install_docker
+install_docker_compose
